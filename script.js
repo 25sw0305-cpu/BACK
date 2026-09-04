e4221142f3cb241b063446939d01bd710aa5befb
25sw0305-cpu <25sw0305@sonline20.sen.go.kr>
2026-09-04 10:50:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,19 @@
 var myName = "김시온";
 var myAge = 18;
 
-input.addEventListener("change", function (){ ... });
-
 var themeInputs = document.querySelectorAll('input[name="theme"]');
 
 var fontInputs = document.querySelectorAll('input[name="fontsize"]');
+
+themeInputs.forEach(function (input) {
+   input.addEventListener("change", function () {
+      // html 태그의 data-theme 값을 다크(dark) 모드로 바꿔라!
+      document.documentElement.setAttribute("data-theme", input.value);
+   });
+});
 
 fontInputs.forEach(function (input) {
    input.addEventListener("change", function () {
       document.documentElement.setAttribute("data-fontsize", input.value);
    });
 });
-
-// html 태그의 data-theme 값을 다크(dark) 모드로 바꿔라!
-document.documentElement.setAttribute("data-theme", input.value);
