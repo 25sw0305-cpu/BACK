8c397d943288f6b50115b3c4b679e84e9440f01a
25sw0305-cpu <25sw0305@sonline20.sen.go.kr>
2026-09-04 11:14:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var myName = "김시온";
+var myAge = 18;
+
+var themeInputs = document.querySelectorAll('input[name="theme"]');
+
+var fontInputs = document.querySelectorAll('input[name="fontsize"]');
+
+var widthInputs = document.querySelectorAll('input[name="width"]');
+
+themeInputs.forEach(function (input) {
+   input.addEventListener("change", function () {
+      // html 태그의 data-theme 값을 선택한 테마로 변경
+      document.documentElement.setAttribute("data-theme", input.value);
+   });
+});
+
+fontInputs.forEach(function (input) {
+   input.addEventListener("change", function () {
+      document.documentElement.setAttribute("data-fontsize", input.value);
+   });
+});
+
+widthInputs.forEach(function (input){
+  input.addEventListener("change", function (){
+    document.documentElement.setAttribute("data-width", input.value);
+  });
+});
