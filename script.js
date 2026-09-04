42373a83e0396ad3320c07d2d0d42c3e2d6fcd5a
25sw0305-cpu <25sw0305@sonline20.sen.go.kr>
2026-09-04 10:43:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
    2차시부터 이 파일에 '외관' 패널을 움직이는 코드를 넣습니다.
    ========================================================= */
 
+var myName = "김시온";
+var myAge = 18;
+
 var fontInputs = document.querySelectorAll('input[name="fontsize"]');
 
 fontInputs.forEach(function (input) {
